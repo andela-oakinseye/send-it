ea6b239bc55c3d25ab1dd60932eb6d65f7bb89d7
andela-oakinseye <olawale.akinseye@andela.com>
2018-11-10 11:12:38 +0100

    set up express

--- server/server.js
+++ server/server.js
@@ -1,5 +1,18 @@
 import 'dotenv/config';
+import cors from 'cors';
+import morgan from 'morgan';
 import logger from 'console';
+import express from 'express';
 
-logger.log('Hello World!');
-logger.log(process.env.ADMIN_PASSWORD);
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('combined'));
+app.use(express.urlencoded({ extended: false }));
+
+
+app.listen(PORT, () => {
+  logger.log(`Running on PORT ${PORT}`);
+});

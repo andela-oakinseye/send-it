7fc7901aa5d8d6c3a6c431b73815fd417eef62fb
andela-oakinseye <olawale.akinseye@andela.com>
2018-11-10 11:55:21 +0100

    setup tests with mocha and chai

--- server/server.js
+++ server/server.js
@@ -2,17 +2,24 @@ import 'dotenv/config';
 import cors from 'cors';
 import morgan from 'morgan';
 import logger from 'console';
-import express from 'express';
+import express, { Router } from 'express';
 
 const app = express();
+const router = Router();
 const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/v1', router);
 app.use(morgan('combined'));
 app.use(express.urlencoded({ extended: false }));
 
+router.get('/', (req, res) => res.json({
+  message: 'Welcome to SendIt API',
+}));
 
 app.listen(PORT, () => {
   logger.log(`Running on PORT ${PORT}`);
 });
+
+export default app;
